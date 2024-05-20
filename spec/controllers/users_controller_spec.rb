a844f569bb1268f61f7ff41128c6f718bcd0bcb7
h8rry <harrychenca@gmail.com>
2024-05-20 16:31:10 -0700

    Revert back to original tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'POST #create' do
-    context 'with valid params' do
-      it 'creates a new user' do
-        post :create, params: { user: { username: 'test', email: 'test@test.com', password: 'secure_password' } }
-        expect(response).to have_http_status(:created)
-        expect(response.body).to eq(User.last.to_json)
-      end
-    end
+  render_views
 
-    context 'with invalid params' do
-      it 'returns unprocessable entity' do
-        post :create, params: { user: { username: 'test' } }
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.body).to include('errors')
-      end
+  describe 'POST /users' do
+    it 'renders new user object' do
+      post :create, params: {
+        user: {
+          email: 'test@test.com',
+          password: 'asdasdasd',
+          username: 'test'
+        }
+      }
+
+      expect(response.body).to eq({
+        user: {
+          username: 'test',
+          email: 'test@test.com'
+        }
+      }.to_json)
     end
   end
 end
